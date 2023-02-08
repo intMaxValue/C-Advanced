73007fee96d1a3db10353dfed50c55f544293694
intMaxValue <106592252+intMaxValue@users.noreply.github.com>
2023-02-09 00:09:05 +0200

    Exam Prep

---
+++ CSharpAdvancedExamPrep/ClimbThePeaks/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ClimbThePeaks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> peaks = new Dictionary<string, int>()
+            {
+                {"Vihren", 80 },
+                {"Kutelo", 90 },
+                {"Banski Suhodol", 100 },
+                {"Polezhan", 60 },
+                {"Kamenitza", 70 }
+            };
+
+            int[] foodInput = Console.ReadLine()
+               .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            Stack<int> food = new Stack<int>(foodInput);
+
+            int[] staminaInput = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> stamina = new Queue<int>(staminaInput);
+
+            List<string> conqueredPeaks = new List<string>();
+
+            for (int i = 0; i < 7; i++)
+            {
+                int sum = 0;
+
+                if (CheckFoodAndStaminaStatus(food, stamina))
+                {
+                    sum = food.Pop() + stamina.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+
+                string currentPeak = string.Empty;
+                int currentPeakDifficulty = 0;
+
+                if (peaks.Any())
+                {
+                    currentPeak = peaks.Keys.First().ToString();
+                    currentPeakDifficulty = peaks.Values.First();
+                }
+                else
+                {
+                    break;
+                }
+
+                if (sum >= currentPeakDifficulty)
+                {
+                    conqueredPeaks.Add(currentPeak);
+                    peaks.Remove(currentPeak);
+
+                    if (CheckFoodAndStaminaStatus(food, stamina) && peaks.Any())
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+            if (!peaks.Any())
+            {
+                Console.WriteLine("Alex did it! He climbed all top five Pirin peaks in one week -> @FIVEinAWEEK");
+                Console.WriteLine("Conquered peaks:");
+                foreach (var peak in conqueredPeaks)
+                {
+                    Console.WriteLine(peak);
+                }
+                return;
+            }
+            else if (peaks.Any() && conqueredPeaks.Count > 0)
+            {
+                Console.WriteLine("Alex failed! He has to organize his journey better next time -> @PIRINWINS");
+                Console.WriteLine("Conquered peaks:");
+                foreach (var peak in conqueredPeaks)
+                {
+                    Console.WriteLine(peak);
+                }
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Alex failed! He has to organize his journey better next time -> @PIRINWINS");
+            }
+        }
+
+
+        private static bool CheckFoodAndStaminaStatus(Stack<int> food, Queue<int> stamina)
+        {
+            if (food.Any() && stamina.Any())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CSharpAdvancedExamPrep/NavyBattle/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace NavyBattle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            int rows = size;
+            int cols = size;
+            string[,] field = new string[rows, cols];
+
+            int u9Row = -1;
+            int u9Col = -1;
+
+            int minesHit = 0;
+            int cruisersSank = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                string line = Console.ReadLine();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    field[row, col] = line[col].ToString();
+
+                    if (line[col].ToString() == "S")
+                    {
+                        u9Row = row;
+                        u9Col = col;
+                        field[row, col] = "-";
+                    }
+                }
+            }
+
+            while (true)
+            {
+                if (minesHit == 3)
+                {
+                    Console.WriteLine($"Mission failed, U-9 disappeared! Last known coordinates [{u9Row}, {u9Col}]!");
+                    break;
+                }
+                if (cruisersSank == 3)
+                {
+                    Console.WriteLine("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+                    break;
+                }
+                int lastRow = u9Row;
+                int lastCol = u9Col;
+
+                string direction = Console.ReadLine().ToLower();
+
+                switch (direction)
+                {
+                    case "up":
+                        u9Row--;
+                        break;
+                    case "down":
+                        u9Row++;
+                        break;
+                    case "left":
+                        u9Col--;
+                        break;
+                    case "right":
+                        u9Col++;
+                        break;
+                }
+
+                if (field[u9Row, u9Col] == "-")
+                {
+                    field[u9Row, u9Col] = "S";
+                    field[lastRow, lastCol] = "-";
+                }
+                else if (field[u9Row, u9Col] == "*")
+                {
+                    minesHit++;
+                    field[u9Row, u9Col] = "S";
+                    field[lastRow, lastCol] = "-";
+                }
+                else if (field[u9Row, u9Col] == "C")
+                {
+                    cruisersSank++;
+                    field[u9Row, u9Col] = "S";
+                    field[lastRow, lastCol] = "-";
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write($"{field[i,j]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
